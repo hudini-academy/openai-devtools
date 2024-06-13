39dc54b23e1619de15948e15d647a3b529eb5323
Vishwa Hm <vishwahm@Vishwas-MacBook-Air-2.local>
2024-06-13 13:04:12 +0530

    Adding users table to the up migrations

--- pkg/migrations/000001_create_openaiusers_schema.up.sql
+++ pkg/migrations/000001_create_openaiusers_schema.up.sql
@@ -4,6 +4,14 @@ CREATE DATABASE IF NOT EXISTS openaiusers;
 -- Use the newly created database
 USE openaiusers;
 
+-- Create the 'users' table to store user information
+CREATE TABLE users (
+    id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT, -- Unique ID for each user
+    name VARCHAR(255) NOT NULL, -- Name of the user
+    email VARCHAR(255) NOT NULL, -- Email of the user
+    password CHAR(60) NOT NULL -- Password of the user
+);
+
 -- Create the 'messages' table to store messages
 CREATE TABLE messages (
     id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT, -- Unique ID for each message
