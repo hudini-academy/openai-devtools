f6f399a3c30f252ee57d479ae8e7441d6d1369e0
Sivanandha02 <sivasajeev2001@gmail.com>
2024-06-14 14:52:32 +0530

    modified frontend, added validation and restriicted access for unauthorised user

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-	"context"
+
 	"net/http"
 )
 
-func (app *application) authenticateMiddleware(next http.Handler) http.Handler {
+func (app *application) AuthenticateMiddleware(next http.Handler) http.Handler {
 	fn := func(w http.ResponseWriter, r *http.Request) {
-		if !app.session.GetBool(r, "Authenticated") {
+		if !app.session.GetBool(r, "Authentication") {
 			app.session.Put(r, "flash", "Log In Before Accessing the resources")
 			http.Redirect(w, r, "/user/login", http.StatusFound)
 			return
@@ -15,14 +15,4 @@ func (app *application) authenticateMiddleware(next http.Handler) http.Handler {
 		next.ServeHTTP(w, r)
 	}
 	return http.HandlerFunc(fn)
-}
-func (app *application) loadUser(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		user, ok := app.session.Get(r, "user").(string)
-		if ok && user != "" {
-			ctx := context.WithValue(r.Context(), "user", user)
-			r = r.WithContext(ctx)
-		}
-		next.ServeHTTP(w, r)
-	})
 }
